0f66cc7540e650ce4f210204fc34f84b8389bf6d
FlorentinaAmalia <florentina.ciublan@s.unibuc.ro>
2024-01-03 13:54:03 +0200

    added carousel to landing page

--- index.js
+++ index.js
@@ -2,7 +2,55 @@ const menuBtn = document.querySelector("nav.navbar .menu");
 const menuCloseBtn = document.querySelector(".menu-close");
 
 const listLink = document.querySelector("nav.navbar ul");
+// const links = listLink.querySelectorAll("a");
+
 let menuClicked = false;
+
+// carousel
+const innerCarousel = document.querySelector(".carousel-inner");
+const imagesCarousel = document.querySelectorAll(".carousel-inner img");
+const nextCarousel = document.querySelector(".next-carousel");
+const prevCarousel = document.querySelector(".prev-carousel");
+
+let currentCarouselImage = 0;
+
+nextCarousel.addEventListener("click", () => {
+    resetInterval();
+    currentCarouselImage++;
+    changeImage();
+});
+
+prevCarousel.addEventListener("click", () => {
+    resetInterval();
+    currentCarouselImage--;
+    changeImage();
+});
+
+function changeImage() {
+    if (currentCarouselImage > imagesCarousel.length - 1) {
+        currentCarouselImage = 0;
+    } else if (currentCarouselImage < 0) {
+        currentCarouselImage = imagesCarousel.length - 1;
+    }
+    innerCarousel.style.transform = `translateX(${
+        -currentCarouselImage * 100
+    }%)`;
+}
+
+let intervalCarousel = setInterval(() => {
+    currentCarouselImage++;
+    changeImage();
+}, 3000);
+
+function resetInterval() {
+    clearInterval(intervalCarousel);
+    intervalCarousel = setInterval(() => {
+        currentCarouselImage++;
+        changeImage();
+    }, 3000);
+}
+
+// carousel end
 
 menuBtn.addEventListener("click", () => {
     showMenu("block");
@@ -12,11 +60,10 @@ menuCloseBtn.addEventListener("click", () => {
 });
 
 function showMenu(display) {
-    if (display == "block") {
-        menuClicked = true;
-    } else {
-        menuClicked = false;
-    }
+    menuClicked = display === "block";
+
+    document.querySelector("section.hero").classList.toggle("menu");
+
     listLink.classList.toggle("hidden");
     menuBtn.classList.toggle("hidden");
     menuCloseBtn.classList.toggle("hidden");
@@ -44,3 +91,13 @@ function changeMenuColumn() {
         listLink.classList.add("hidden");
     }
 }
+
+// for (let link of links) {
+//     link.addEventListener("click", closeMenu);
+// }
+
+// function closeMenu() {
+//     if (window.innerWidth < 1050) {
+//         showMenu("none");
+//     }
+// }
